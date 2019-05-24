519f43dc9a91b4a1b5bf0f94aa51d94c0f44988a
Daniel Nowosad <daniel.nowosad89@gmail.com>
2019-05-25 01:09:07 +0200

    1.1

--- index.php
+++ index.php
@@ -5,23 +5,26 @@
 <html lang="pl" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="STYLE.CSS">
     <title>danek</title>
   </head>
   <body>
-<table>
-  <tr>
+<table id="TABELKA">
+  <tr id="TABELKA">
     <td>danek</td>
   </tr>
   <tr>
     <td>danek 2</td>
     <td>danek 2</td>
+    <td>TUCOS BEDZIE.<?php ECHO $_POST['dan']?></td>
   </tr>
 </table>
 <form class="" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
   <input type="text" name="dan" value="">
-  <input type="submit" name="danek" value="Wyslij dane">
-  <?php if (isset $_POST[danek])) {
-    echo $_POST[dan];
+  <input type="submit" name="danek" value="Wyslij dane"><br>
+
+  <?php if (isset ($_POST['danek'])) {
+    echo $_POST['dan'];
   } ?>
 </form>
   </body>
